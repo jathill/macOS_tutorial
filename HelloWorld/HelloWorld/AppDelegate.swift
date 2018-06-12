f58793ff54830c31f45900531d37323c775ec41b
Justin Athill <jcathill@gmail.com>
2018-06-12 10:50:03 -0700

    part 1: hello world

---
+++ HelloWorld/HelloWorld/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HelloWorld
+//
+//  Created by Justin Athill on 6/12/18.
+//  Copyright © 2018 Justin_Athill. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Justin Athill on 6/12/18.
+//  Copyright © 2018 Justin_Athill. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @IBOutlet weak var nameField: NSTextField!
+    @IBOutlet weak var helloLabel: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+    
+    @IBAction func sayButtonClicked(sender: AnyObject) {
+        var name = nameField.stringValue
+        if name.isEmpty {
+            name = "World"
+        }
+        let greeting = "Hello \(name)!"
+        helloLabel.stringValue = greeting
+    }
+
+
+}
+
